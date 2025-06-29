02447090d0e8426b031fb6dcc60b2ef5d5cd293b
Viet Hung Nguyen <vhn@sfu.ca>
2025-06-29 06:44:38 -0700

    Allow horizontal scrolling when rendering with highlighting

--- src/v2/ui/components/buffer_view.rs
+++ src/v2/ui/components/buffer_view.rs
@@ -1,10 +1,11 @@
-use std::str::from_utf8;
 use crate::ui::render_buffer::RenderBuffer;
+use crate::ui::theme::Style;
 use crate::ui::{Bounds, Drawable, RenderContext};
 use anyhow::{Ok, Result};
 use log::info;
+use std::ops::Add;
+use std::str::from_utf8;
 use tree_sitter::Point;
-use crate::ui::theme::Style;
 
 pub struct BufferView {
     id: String,
@@ -76,8 +77,11 @@ impl BufferView {
         let editor_style = theme.editor_style();
 
         let code = buffer.to_bytes();
-        let tokens = context.buffer_manager.get_syntax_highlighter().highlight(&code)?;
-        
+        let tokens = context
+            .buffer_manager
+            .get_syntax_highlighter()
+            .highlight(&code)?;
+
         info!("{tokens:?}");
 
         let top_line = viewport.top_line();
@@ -87,7 +91,8 @@ impl BufferView {
         let mut info_iter = tokens
             .iter()
             .filter(|info| {
-                info.end_position.row >= top_line && info.start_position.row < top_line + visible_height as usize
+                info.end_position.row >= top_line
+                    && info.start_position.row < top_line + visible_height as usize
             })
             .map(|info| {
                 let mut new_info = info.clone();
@@ -97,8 +102,6 @@ impl BufferView {
             })
             .peekable();
 
-        let mut position = tree_sitter::Point { row: 0, column: 0 };
-
         // Render the first unhighlighted part of the code
         let first = if let Some(info) = info_iter.peek() {
             &code[..info.byte_range.start]
@@ -106,34 +109,34 @@ impl BufferView {
             &code
         };
 
-        let mut lines = first
-            .split(|&b| b == b'\n')
-            .skip(top_line)
-            .peekable();
+        let mut lines = first.split(|&b| b == b'\n').skip(top_line).peekable();
+
+        let mut position = tree_sitter::Point { row: 0, column: 0 };
 
         while let Some(line) = lines.next() {
             let text = from_utf8(line)?;
+
+            for c in text.chars() {
+                if position.column >= left_column {
+                    render_buffer.set_cell(
+                        position.row,
+                        position.column - left_column + start_col,
+                        c,
+                        &editor_style,
+                    );
+                }
+                position.column += 1;
+            }
+
             if lines.peek().is_some() {
-                let formatted = format!("{text:<w$}", w = visible_width as usize);
                 render_buffer.set_text(
                     position.row,
-                    position.column + start_col,
-                    &formatted,
+                    position.column.saturating_sub(left_column).add(start_col),
+                    &" ".repeat(visible_width),
                     &editor_style,
                 );
-                if position.row + 1 >= visible_height {
-                    break;
-                }
                 position.row += 1;
                 position.column = 0;
-            } else {
-                render_buffer.set_text(
-                    position.row,
-                    position.column + start_col,
-                    text,
-                    &editor_style,
-                );
-                position.column += text.len();
             }
         }
 
@@ -185,14 +188,13 @@ impl BufferView {
         while position.row < visible_height as usize {
             render_buffer.set_text(
                 position.row,
-                position.column + start_col,
+                position.column.saturating_sub(left_column).add(start_col),
                 &empty,
                 &editor_style,
             );
             position.row += 1;
             position.column = 0;
         }
-
 
         Ok(())
     }
@@ -205,24 +207,43 @@ impl BufferView {
         bytes: &[u8],
         style: &Style,
     ) -> Result<()> {
-        let Bounds { start_col: gutter, width, height, .. }
-            = self.bounds(render_buffer.get_size(), context);
+        let Bounds {
+            start_col,
+            width: visible_width,
+            height,
+            ..
+        } = self.bounds(render_buffer.get_size(), context);
+        let left_column = context.viewport.left_column();
 
         let mut lines = bytes.split(|&c| c == b'\n').peekable();
 
         while let Some(line) = lines.next() {
-            let text = from_utf8(&line)?;
+            let text = from_utf8(line)?;
+
+            for c in text.chars() {
+                if position.column >= left_column {
+                    render_buffer.set_cell(
+                        position.row,
+                        position.column - left_column + start_col,
+                        c,
+                        &style,
+                    );
+                }
+                position.column += 1;
+            }
+
             if lines.peek().is_some() {
-                let text = format!("{text:<w$}", w = width as usize);
-                render_buffer.set_text(position.row, position.column + gutter, &text, style);
+                render_buffer.set_text(
+                    position.row,
+                    position.column.saturating_sub(left_column).add(start_col),
+                    &" ".repeat(visible_width),
+                    &style,
+                );
                 if position.row + 1 >= height as usize {
                     break;
                 }
                 position.row += 1;
                 position.column = 0;
-            } else {
-                render_buffer.set_text(position.row, position.column + gutter, text, style);
-                position.column += text.len();
             }
         }
         Ok(())
