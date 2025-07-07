444d78fe87ed08fc44668866aad97980fd9df362
Viet Hung Nguyen <vhn@sfu.ca>
2025-07-07 01:33:11 -0700

    Remove unused methods in Utf8CharIterator

--- src/core/utf8.rs
+++ src/core/utf8.rs
@@ -20,56 +20,6 @@ impl<'a> Utf8CharIterator<'a> {
             char_pos: 0,
         }
     }
-
-    pub fn from_byte_pos(bytes: &'a [u8], start_byte_pos: usize) -> Self {
-        let mut iter = Self::new(bytes);
-        
-        // Fast-forward to the starting position
-        while iter.byte_pos < start_byte_pos && iter.byte_pos < bytes.len() {
-            if iter.next().is_none() {
-                break;
-            }
-        }
-        
-        iter
-    }
-
-    pub fn from_char_pos(bytes: &'a [u8], start_char_pos: usize) -> Self {
-        let mut iter = Self::new(bytes);
-        
-        // Fast-forward to the starting position
-        while iter.char_pos < start_char_pos {
-            if iter.next().is_none() {
-                break;
-            }
-        }
-        
-        iter
-    }
-
-    pub fn byte_pos_for_char_pos(bytes: &'a [u8], target_char_pos: usize) -> Option<usize> {
-        let mut iter = Self::new(bytes);
-        
-        while let Some(char_pos) = iter.next() {
-            if char_pos.char_index >= target_char_pos {
-                return Some(char_pos.byte_index);
-            }
-        }
-        
-        None
-    }
-
-    pub fn char_pos_for_byte_pos(bytes: &'a [u8], target_byte_pos: usize) -> Option<usize> {
-        let mut iter = Self::new(bytes);
-        
-        while let Some(char_pos) = iter.next() {
-            if char_pos.byte_index >= target_byte_pos {
-                return Some(char_pos.char_index);
-            }
-        }
-        
-        None
-    }
 }
 
 impl<'a> Iterator for Utf8CharIterator<'a> {
@@ -82,7 +32,7 @@ impl<'a> Iterator for Utf8CharIterator<'a> {
 
         let start_byte = self.byte_pos;
         let first_byte = self.bytes[self.byte_pos];
-        
+
         let (char_len, char_value) = if first_byte < 0x80 {
             // ASCII character
             (1, first_byte as char)
@@ -126,14 +76,14 @@ impl<'a> Iterator for Utf8CharIterator<'a> {
                 Err(_) => (1, '\u{FFFD}'),
             }
         };
-        
+
         let position = CharPosition {
             character: char_value,
             char_index: self.char_pos,
             byte_index: start_byte,
             byte_len: char_len,
         };
-        
+
         self.byte_pos += char_len;
         self.char_pos += 1;
 
